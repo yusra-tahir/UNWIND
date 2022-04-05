8ba02fce88f9df1f02915a60a1e1e0397c6c8a01
Yusra <yusra-tahir@hotmail.co.uk>
2022-04-05 10:07:50 +0100

    updated server

--- server/app.js
+++ server/app.js
@@ -4,14 +4,46 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { json } = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
+// ---------------  REQUIRE ARRAY FROM JSON FILE  --------------- //
 
-//---------------- GIPHY --------------------------//
-function sendApiRequest(){
+const postMessage = require('./database.json');
+
+
+// --------------- CHANGE POSTS SUBMITTED FROM FRONTEND TO JSON AND ADD TO database.json --------------- //
+
+const saveData = () => {
+    const finished = (error) => {
+        if (error) {
+            console.error(err);
+            return;
+        }
+    }
+    const jsonData = JSON.stringify(postMessage, null, 2);
+    fs.writeFile('database.json', jsonData, finished);
+};
+
+// [null, 2] makes the file more readable in the json file 
+// writes json stringified data to database.json and run error function otherwise
+
+
+// ----------------------------- PUSHING THE FORM DATA FROM FRONTEND INTO FORMDATA VARIABLE AND WRITING THE FORMDATA VARIABLE BACK INTO input.json --------------------------------
+
+app.post('/test', (req, res) => {
+    formData.push(req.body)
+    // console.log(formData)
+    writeToJson();
+    res.json({success: true})
+})
+
+
+// --------------- GIPHY --------------- //
+
+function sendApiRequest() {
     const userInput = document.getElementById("input").value
 
     console.log(userInput);
@@ -37,9 +69,21 @@ function sendApiRequest(){
         })
 }
 
+// --------------- ROUTES --------------- //
+
+
+// main endpoint
+app.get('/', (req, res) => {
+    res.send(console.log('SERVERSIDE DEPLOYED!'));
+});
+
+// posts endpoint
+app.get('/posts', (req, res) => {
+    res.send(postMessage);
+});
 
 // --------------- LISTEN TO SERVER --------------- //
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Example App on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`App on ${port}`);
 });
